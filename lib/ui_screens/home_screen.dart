61cb087e56542e2e784dae8df313ce3ab3d37b96
Faysal Zaman <123411654+faysalzaman@users.noreply.github.com>
2024-11-03 11:40:18 +0500

    Updated

--- lib/ui_screens/home_screen.dart
+++ lib/ui_screens/home_screen.dart
@@ -209,7 +209,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       valueColor:
                           AlwaysStoppedAnimation<Color>(Colors.purple)));
             } else if (state is NamesSuccess) {
-              namemodel = state.filteredList!;
+              namemodel = state.filteredList ?? [];
               if (filteredNames.isEmpty) filteredNames = List.from(namemodel);
               _updateAlphabet();
 
